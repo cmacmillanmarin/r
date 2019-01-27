46fc798470d93769cc31d201b962c43f5f43be78
ariiiman <aristide.benoist@gmail.com>
2019-01-27 10:42:56 +0100

    0.0.6

--- build/bash/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-node build/node/concat.js
-node build/node/uglify.js
-rm -r r.js
-node build/node/version.js
-sh build/bash/copy.sh
--- build/node/uglify.js
+++
@@ -1,7 +0,0 @@
-const Uglify = require('uglify-js')
-const fs = require('fs')
-
-const js = fs.readFileSync('r.js', 'utf8')
-const result = Uglify.minify(js).code
-
-fs.writeFileSync('index.js', result, 'utf8')
