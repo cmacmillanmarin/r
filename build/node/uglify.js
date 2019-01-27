2066c993037e8765978993d33904f6f0b4b05c5b
ariiiman <aristide.benoist@gmail.com>
2019-01-27 10:41:51 +0100

    0.0.6

--- build/node/uglify.js
+++ build/node/uglify.js
@@ -1,7 +1,7 @@
-const UglifyJS = require('uglify-js')
+const Uglify = require('uglify-js')
 const fs = require('fs')
 
 const js = fs.readFileSync('r.js', 'utf8')
-const result = UglifyJS.minify(js).code
+const result = Uglify.minify(js).code
 
 fs.writeFileSync('index.js', result, 'utf8')
